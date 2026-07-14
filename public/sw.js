149bec537929f510e6749c7758f1fd95f398b80f
Con Co <coder-developer@users.noreply.github.com>
2026-07-15 02:52:47 +0700

    Làm mới bộ nhớ đệm PWA

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'construct-os-v1';
+const CACHE = 'quan-tri-doanh-nghiep-v2';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/','/manifest.webmanifest','/icon.svg']))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => {
